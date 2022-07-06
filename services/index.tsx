45dd24c3784b60d2685ef2ada243319ff7d1a260
Edmilson Ferreira <edmilsonf.o3@gmail.com>
2022-07-06 16:05:03 -0300

    🚀 :rocket: [Add] getRecentPosts

---
+++ services/index.tsx
@@ -0,0 +1,61 @@
+import request, { gql } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT as string;
+
+export const getPosts = async () => {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            author {
+              name
+              bio
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.postsConnection.edges;
+};
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      )
+    } {
+      title
+      featuredImage {
+        url
+      }
+      createdAt
+      slug
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
