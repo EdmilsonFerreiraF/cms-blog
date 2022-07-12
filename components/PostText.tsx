151a8aa063db1b538021f6fc0d5dd3824e8eb0dc
Edmilson Ferreira <edmilsonf.o3@gmail.com>
2022-07-12 17:50:35 -0300

    🎨 :art: [Refactor] Encapsulate switchContentType

--- components/PostText.tsx
+++ components/PostText.tsx
@@ -9,11 +9,60 @@ type Props = {
 };
 
 const PostText = ({ postText }: Props) => {
+  const switchContentType = (
+    modifiedText: any,
+    index: number,
+    obj: any,
+    type?: "heading-three" | "paragraph" | "heading-four" | "image"
+  ) => {
+    if (type === "heading-three") {
+      return (
+        <h3 key={index} className="text-xl font-semibold mb-4">
+          {modifiedText.map((item: any, i: number) => (
+            <React.Fragment key={i}>{item}</React.Fragment>
+          ))}
+        </h3>
+      );
+    }
+    if (type === "paragraph") {
+      return (
+        <p key={index} className="mb-8">
+          {modifiedText.map((item: any, i: number) => (
+            <React.Fragment key={i}>{item}</React.Fragment>
+          ))}
+        </p>
+      );
+    }
+    if (type === "heading-four") {
+      return (
+        <h4 key={index} className="text-md font-semibold mb-4">
+          {modifiedText.map((item: any, i: number) => (
+            <React.Fragment key={i}>{item}</React.Fragment>
+          ))}
+        </h4>
+      );
+    }
+    if (type === "image") {
+      return (
+        <Image
+          loader={graphCMSImageLoader}
+          key={index}
+          alt={obj.title}
+          height={obj.height}
+          width={obj.width}
+          src={obj.src}
+        />
+      );
+    }
+
+    return modifiedText;
+  };
+
   const getContentFragment = (
     index: number,
     text: string,
     obj: any,
-    type?: string
+    type?: "heading-three" | "paragraph" | "heading-four" | "image"
   ) => {
     let modifiedText: any = text;
 
@@ -31,45 +80,7 @@ const PostText = ({ postText }: Props) => {
       }
     }
 
-    switch (type) {
-      case "heading-three":
-        return (
-          <h3 key={index} className="text-xl font-semibold mb-4">
-            {modifiedText.map((item: any, i: number) => (
-              <React.Fragment key={i}>{item}</React.Fragment>
-            ))}
-          </h3>
-        );
-      case "paragraph":
-        return (
-          <p key={index} className="mb-8">
-            {modifiedText.map((item: any, i: number) => (
-              <React.Fragment key={i}>{item}</React.Fragment>
-            ))}
-          </p>
-        );
-      case "heading-four":
-        return (
-          <h4 key={index} className="text-md font-semibold mb-4">
-            {modifiedText.map((item: any, i: number) => (
-              <React.Fragment key={i}>{item}</React.Fragment>
-            ))}
-          </h4>
-        );
-      case "image":
-        return (
-          <Image
-            loader={graphCMSImageLoader}
-            key={index}
-            alt={obj.title}
-            height={obj.height}
-            width={obj.width}
-            src={obj.src}
-          />
-        );
-      default:
-        return modifiedText;
-    }
+    switchContentType(modifiedText, index, obj, type);
   };
 
   return (
@@ -78,6 +89,7 @@ const PostText = ({ postText }: Props) => {
         const children = typeObj.children.map((item: any, itemIndex: any) =>
           getContentFragment(itemIndex, item.text, item)
         );
+
         return getContentFragment(index, children, typeObj, typeObj.type);
       })}
     </>
