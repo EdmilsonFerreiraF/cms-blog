12a59ff7e4731c8fd1676284f716e1b2b7854d6a
Edmilson Ferreira <edmilsonf.o3@gmail.com>
2022-07-12 15:54:31 -0300

    🎨 :art: [Refactor] Encapsulate PostText

--- components/PostDetail.tsx
+++ components/PostDetail.tsx
@@ -1,9 +1,9 @@
-import moment from "moment";
 import Image from "next/image";
-import React from "react";
 import { graphCMSImageLoader } from "../util";
 import Author from "./Author";
 import { Category } from "./Header";
+import PostDate from "./PostDate";
+import PostText from "./PostText";
 
 export type PostDetailType = {
   author: Author;
@@ -16,7 +16,9 @@ export type PostDetailType = {
   };
   categories: Category[];
   content: {
-    raw: any;
+    raw: {
+      children: JSX.Element[];
+    } & any;
   };
 };
 
@@ -25,69 +27,6 @@ type Props = {
 };
 
 const PostDetail = ({ post }: Props) => {
-  const getContentFragment = (
-    index: number,
-    text: string,
-    obj: any,
-    type?: string
-  ) => {
-    let modifiedText: any = text;
-
-    if (obj) {
-      if (obj.bold) {
-        modifiedText = <b key={index}>{text}</b>;
-      }
-
-      if (obj.italic) {
-        modifiedText = <em key={index}>{text}</em>;
-      }
-
-      if (obj.underline) {
-        modifiedText = <u key={index}>{text}</u>;
-      }
-    }
-
-    switch (type) {
-      case "heading-three":
-        return (
-          <h3 key={index} className="text-xl font-semibold mb-4">
-            {modifiedText.map((item: any, i: number) => (
-              <React.Fragment key={i}>{item}</React.Fragment>
-            ))}
-          </h3>
-        );
-      case "paragraph":
-        return (
-          <p key={index} className="mb-8">
-            {modifiedText.map((item: any, i: number) => (
-              <React.Fragment key={i}>{item}</React.Fragment>
-            ))}
-          </p>
-        );
-      case "heading-four":
-        return (
-          <h4 key={index} className="text-md font-semibold mb-4">
-            {modifiedText.map((item: any, i: number) => (
-              <React.Fragment key={i}>{item}</React.Fragment>
-            ))}
-          </h4>
-        );
-      case "image":
-        return (
-          <Image
-            loader={graphCMSImageLoader}
-            key={index}
-            alt={obj.title}
-            height={obj.height}
-            width={obj.width}
-            src={obj.src}
-          />
-        );
-      default:
-        return modifiedText;
-    }
-  };
-
   return (
     <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
       <div className="relative overflow-hidden shadow-md mb-6">
@@ -104,41 +43,19 @@ const PostDetail = ({ post }: Props) => {
               loader={graphCMSImageLoader}
               src={post.author.photo.url}
               alt={post.author.name}
+              unoptimized
               height="30px"
               width="30px"
-              unoptimized
               className="align-middle rounded-full"
             />
             <p className="inline align-middle text-gray-700 ml-2 text-lg">
               {post.author.name}
             </p>
           </div>
-          <div className="font-medium text-gray-700">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 inline mr-2 text-pink-500"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-              />
-            </svg>
-            <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
-          </div>
+          <PostDate postDate={post.createdAt} />
         </div>
         <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
-
-        {post.content.raw.children.map((typeObj: any, index: number) => {
-          const children = typeObj.children.map((item: any, itemIndex: any) =>
-            getContentFragment(itemIndex, item.text, item)
-          );
-          return getContentFragment(index, children, typeObj, typeObj.type);
-        })}
+        <PostText postText={post.content.raw} />
       </div>
     </div>
   );
---
+++ components/PostText.tsx
@@ -0,0 +1,87 @@
+import Image from "next/image";
+import React from "react";
+import { graphCMSImageLoader } from "../util";
+
+type Props = {
+  postText: {
+    children: JSX.Element[];
+  } & any;
+};
+
+const PostText = ({ postText }: Props) => {
+  const getContentFragment = (
+    index: number,
+    text: string,
+    obj: any,
+    type?: string
+  ) => {
+    let modifiedText: any = text;
+
+    if (obj) {
+      if (obj.bold) {
+        modifiedText = <b key={index}>{text}</b>;
+      }
+
+      if (obj.italic) {
+        modifiedText = <em key={index}>{text}</em>;
+      }
+
+      if (obj.underline) {
+        modifiedText = <u key={index}>{text}</u>;
+      }
+    }
+
+    switch (type) {
+      case "heading-three":
+        return (
+          <h3 key={index} className="text-xl font-semibold mb-4">
+            {modifiedText.map((item: any, i: number) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h3>
+        );
+      case "paragraph":
+        return (
+          <p key={index} className="mb-8">
+            {modifiedText.map((item: any, i: number) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </p>
+        );
+      case "heading-four":
+        return (
+          <h4 key={index} className="text-md font-semibold mb-4">
+            {modifiedText.map((item: any, i: number) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h4>
+        );
+      case "image":
+        return (
+          <Image
+            loader={graphCMSImageLoader}
+            key={index}
+            alt={obj.title}
+            height={obj.height}
+            width={obj.width}
+            src={obj.src}
+          />
+        );
+      default:
+        return modifiedText;
+    }
+  };
+
+  return (
+    <>
+      {postText.children.map((typeObj: any, index: number) => {
+        const children = typeObj.children.map((item: any, itemIndex: any) =>
+          getContentFragment(itemIndex, item.text, item)
+        );
+        return getContentFragment(index, children, typeObj, typeObj.type);
+      })}
+    </>
+  );
+};
+
+export default PostText;
