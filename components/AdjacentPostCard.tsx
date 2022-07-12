dcf7bb3719567e763533c5a183e18a985babdea2
Edmilson Ferreira <edmilsonf.o3@gmail.com>
2022-07-12 15:24:14 -0300

    🎨 :art: [Refactor] Encapsulate PostTitle

--- components/AdjacentPostCard.tsx
+++ components/AdjacentPostCard.tsx
@@ -1,9 +1,8 @@
-import moment from "moment";
 import Link from "next/link";
-import React from "react";
 import { Post } from "../pages";
 import LeftArrowBtn from "./LeftArrowBtn";
 import PostImage from "./PostImage/PostImage";
+import PostTitle from "./PostTitle";
 import RightArrowBtn from "./RightArrowBtn";
 
 type Props = {
@@ -16,14 +15,7 @@ const AdjacentPosts = ({ post, position }: Props) => {
     <div className="relative carousel-button-group">
       <div className="relative h-72 mb-14">
         <PostImage postImage={post.featuredImage.url} />
-        <Link href={post.slug}>
-          <div className="absolute cursor-pointer text-white top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%] text-white-100 text-center font-semibold translate[-50%]">
-            <span className="text-xs font-semibold mb-4">
-              {moment(post.publishedAt).format("MMM DD, YYYY")}
-            </span>
-            <h5 className="text-2xl">{post.title}</h5>
-          </div>
-        </Link>
+        <PostTitle post={post} />
         <Link href={`/post/${post.slug}`}>
           <div>
             <span className="z-10 cursor-pointer absolute w-full h-full" />
---
+++ components/PostTitle.tsx
@@ -0,0 +1,22 @@
+import moment from "moment";
+import Link from "next/link";
+import { Post } from "../pages";
+
+type Props = {
+  post: Post;
+};
+
+const PostTitle = ({ post }: Props) => {
+  return (
+    <Link href={post.slug}>
+      <div className="absolute cursor-pointer text-white top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%] text-white-100 text-center font-semibold translate[-50%]">
+        <span className="text-xs font-semibold mb-4">
+          {moment(post.publishedAt).format("MMM DD, YYYY")}
+        </span>
+        <h5 className="text-2xl">{post.title}</h5>
+      </div>
+    </Link>
+  );
+};
+
+export default PostTitle;
