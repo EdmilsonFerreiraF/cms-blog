25f943ba9927575e86a0d6d7e61c6a21beab2614
Edmilson Ferreira <edmilsonf.o3@gmail.com>
2022-07-12 11:23:40 -0300

    🎨 :art: [Refactor] Encapsulate CustomLeftBtn

--- components/ButtonGroup.tsx
+++ components/ButtonGroup.tsx
@@ -1,14 +1,9 @@
 import { ButtonGroupProps, CarouselInternalState } from "react-multi-carousel";
+import CustomLeftBtn from "./CustomLeftBtn";
 
 const ButtonGroup = ({ next, previous, ...rest }: ButtonGroupProps) => {
   const carouselState = rest.carouselState as CarouselInternalState;
   const currentSlide = carouselState.currentSlide;
-
-  const handlePreviousClick = () => {
-    const scrollPrevious = previous as () => void;
-
-    scrollPrevious();
-  };
 
   const handleNextClick = () => {
     const scrollNext = previous as () => void;
@@ -18,31 +13,7 @@ const ButtonGroup = ({ next, previous, ...rest }: ButtonGroupProps) => {
 
   return (
     <div className="carousel-button-group">
-      <button
-        className={
-          currentSlide === 0
-            ? "disable"
-            : "text-white arrow-btn bg-pink-600 rounded-full absolute left-0 p-3 top-1/2 translate-y-[-50%] flex justify-center"
-        }
-        onClick={() => handlePreviousClick}
-      >
-        {currentSlide > 0 && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M10 19l-7-7m0 0l7-7m-7 7h18"
-            />
-          </svg>
-        )}
-      </button>
+      <CustomLeftBtn currentSlide={currentSlide} previous={previous} />
       <button
         className="text-white arrow-btn bg-pink-600 rounded-full absolute right-0 p-3 top-1/2 translate-y-[-50%] flex justify-center"
         onClick={() => handleNextClick}
---
+++ components/CustomLeftBtn.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+type Props = {
+  currentSlide: number;
+  previous: (() => void) | undefined;
+};
+
+const CustomLeftBtn = ({ currentSlide, previous }: Props) => {
+  const handlePreviousClick = () => {
+    const scrollPrevious = previous as () => void;
+
+    scrollPrevious();
+  };
+
+  return (
+    <button
+      className={
+        currentSlide === 0
+          ? "disable"
+          : "text-white arrow-btn bg-pink-600 rounded-full absolute left-0 p-3 top-1/2 translate-y-[-50%] flex justify-center"
+      }
+      onClick={() => handlePreviousClick}
+    >
+      {currentSlide > 0 && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M10 19l-7-7m0 0l7-7m-7 7h18"
+          />
+        </svg>
+      )}
+    </button>
+  );
+};
+
+export default CustomLeftBtn;
